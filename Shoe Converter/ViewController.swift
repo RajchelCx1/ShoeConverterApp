5493b7a2c29b23f4f652f64fa3ca6c2a864b92c6
RajchelCx1 <RajchelCx1@gcc.edu>
2015-06-04 20:39:47 -0400

    First Commit...

--- Shoe Converter/ViewController.swift
+++ Shoe Converter/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mensShoeSizeTextField: UITextField!
+    @IBOutlet weak var mensConvertedShoeSizeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func converButtonPressed(sender: UIButton) {
+        
+        //creates our conversion constant
+        let conversionConstant = 30;
+        
+        /*
+        //gets the string from the textfield
+        let sizeFromTextField = mensShoeSizeTextField.text
+        //converts the string into an optional int
+        let numberFromTextField = sizeFromTextField.toInt()
+        //unwraps the optional int
+        var integerFromTextField = numberFromTextField!
+        
+        //conversion...
+        integerFromTextField+=conversionConstant
+        //unhides the label
+        mensConvertedShoeSizeLabel.hidden = false
+        //changes the int back into a string...
+        let stringWithUpdatedShoeSize = "EU Size: \(integerFromTextField)"
+        //updates labels text using the string
+        mensConvertedShoeSizeLabel.text = stringWithUpdatedShoeSize
+        */
+
+        //condensed version of whats above
+        let sizeFromTextField = (mensShoeSizeTextField.text.toInt())!
+        mensConvertedShoeSizeLabel.text = "\(sizeFromTextField + conversionConstant)"
+        mensConvertedShoeSizeLabel.hidden = false
+        
+        //hides keyboard
+        mensShoeSizeTextField.resignFirstResponder()
+        
+    }
 
 }
 
